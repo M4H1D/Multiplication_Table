789b668f62f55a846f6c6a9224d7555a479def88
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2022-05-10 03:45:24 +0600

    Add files via upload

---
+++ MateenSir.py
@@ -0,0 +1,15 @@
+x=""
+while True:
+    n=int(input("Enter a number: "))
+    for i in range(1,11):
+        print(n,"X",i,"=",i*n)
+    x=input("Do you want to exit??(Y/N): ")
+    if x=="Y":
+        continue
+    elif x=="N":
+        print("Bye Bye...")
+        break
+    else:
+        print("error")
+        print("Please run the code again & type correct number...")
+        break
